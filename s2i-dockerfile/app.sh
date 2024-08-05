f390f72258ecadc0ba4c67782d5d8113c5297dc1
John Easton <JKJ@uk.ibm.com>
2024-08-05 09:52:33 +0100

    Test of sh

---
+++ s2i-dockerfile/app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Test app
+#
+echo "This is my test app"
+echo "This is my test app"
+echo "This is my test app"
+echo "This is my test app"
+echo "This is my test app"
+env
+date
+exit 0
