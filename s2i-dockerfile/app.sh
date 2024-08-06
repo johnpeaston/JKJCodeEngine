9e1446a416c7e380f12baf3f7aea8a01b04a99fe
John Easton <JKJ@uk.ibm.com>
2024-08-06 10:32:41 +0100

    Update app.sh

--- s2i-dockerfile/app.sh
+++ s2i-dockerfile/app.sh
@@ -7,4 +7,36 @@ echo "This is my test app"
 echo "This is my test app"
 echo "This is my test app"
 echo "This is my test app"
+
+env
+
+INDEX=13
+
+# Cleanup
+echo "Cleanup..."
+rm -f $HOME/Downloads/DATAFILE
+
+# Get datafile from COS
+echo "Get datafile from COS bucket"
+ibmcloud cos object-get --bucket jkj-hpc-cos-bucket1 --key DATAFILE  --region us-east
+
+# check datafile is OK
+# echo "cat datafile..."
+# cat $HOME/Downloads/DATAFILE
+
+# Process datafile
+echo "Processing datafile..."
+while read -r line
+do
+        # echo $line
+        # sleep 1
+        LINE=`echo $line | cut -f1 -d','`
+        # echo $LINE
+        if [[ $LINE == $INDEX ]]
+        then
+                INPUTDATA=`echo $line | cut -f2 -d','`
+                echo $INPUTDATA
+        fi
+done < $HOME/Downloads/DATAFILE
+
 exit 0
