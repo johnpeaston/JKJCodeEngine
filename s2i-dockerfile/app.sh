d90fadf3959bd617080725e3124181619c57e628
John Easton <JKJ@uk.ibm.com>
2024-08-07 12:04:39 +0100

    remove force

--- s2i-dockerfile/app.sh
+++ s2i-dockerfile/app.sh
@@ -15,7 +15,7 @@ export BUCKET="jkj-hpc-cos-bucket1"
 CID="crn:v1:bluemix:public:cloud-object-storage:global:a/b5e2b6b228eb4f2499d46b73cb1c5db9:f420cab2-83e9-4d93-b37a-41eb5c64dced::"
 # Set the COS config to use this instance
 ibmcloud cos config crn --crn $CID --force
-ibmcloud cos config auth --method IAM --force
+ibmcloud cos config auth --method IAM 
 
 # Cleanup
 #echo "Cleanup..."
