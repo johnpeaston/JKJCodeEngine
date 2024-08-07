4ad3e5b257763d56e0f35cc2748ad501e149e7aa
John Easton <JKJ@uk.ibm.com>
2024-08-07 12:02:46 +0100

    cos login

--- s2i-dockerfile/app.sh
+++ s2i-dockerfile/app.sh
@@ -15,7 +15,7 @@ export BUCKET="jkj-hpc-cos-bucket1"
 CID="crn:v1:bluemix:public:cloud-object-storage:global:a/b5e2b6b228eb4f2499d46b73cb1c5db9:f420cab2-83e9-4d93-b37a-41eb5c64dced::"
 # Set the COS config to use this instance
 ibmcloud cos config crn --crn $CID --force
-ibmcloud cos config auth --method IAM
+ibmcloud cos config auth --method IAM --force
 
 # Cleanup
 #echo "Cleanup..."
@@ -52,7 +52,7 @@ done < /go/DATAFILE
 
 # Put output datafile to COS
 # echo "Put output datafile to COS..."
-#ibmcloud cos object-put --bucket ${BUCKET} --key Output.$JOB_INDEX --body ${OUTPUTDATAFILE}  --region us-east
+ibmcloud cos object-put --bucket ${BUCKET} --key Output.$JOB_INDEX --body ${OUTPUTDATAFILE}  --region us-east
 
 exit 0
 
