e3bb29f4ea464ebf7dbe11972bb84f6086ada195
John Easton <JKJ@uk.ibm.com>
2024-09-20 10:54:31 +0100

    added vol handling

--- s2i-dockerfile/app.working.sh
+++ s2i-dockerfile/app.working.sh
@@ -18,50 +18,6 @@ INPUTDATAFILE=/go/DATAFILE
 # rm -f /go/DATAFILE
 rm -f $OUTPUTDATAFILE
 
-# Setup COS volume mount
-# Usage: add-volume-mount-to-job.sh CE_JOB_NAME MOUNT_NAME MOUNT_PATH
-CE_JOB_NAME=jkj-hpc-cosfs1
-MOUNT_NAME=jkj-hpc-cos-bucket1
-MOUNT_PATH=/mnt
-
-set -euo pipefail
-
-echo ""
-echo "CE_JOB_NAME: '$CE_JOB_NAME'"
-echo "CE_MOUNT_NAME: '$MOUNT_NAME'"
-echo "CE_MOUNT_PATH: '$MOUNT_PATH'"
-echo ""
-
-VOLUME_NAME=$1-$2
-
-# Create the JSON patch
-
-PATCH='[
-  {
-    "op": "add",
-    "path": "/spec/template/volumes",
-    "value": [{
-      "name": "'$VOLUME_NAME'",
-      "persistentVolumeClaim": {
-        "claimName": "'$MOUNT_NAME'"
-      }
-    }]
-  },
-  {
-    "op": "add",
-    "path": "/spec/template/containers/0/volumeMounts",
-    "value": [{
-      "name": "'$VOLUME_NAME'",
-      "mountPath": "'$MOUNT_PATH'"
-    }]
-  }
-]'
-
-# Apply the patch
-kubectl patch jobdefinition  $CE_JOB_NAME --type='json' --patch "$PATCH"
-
-echo "Patched job '$CE_JOB_NAME' with volume '$VOLUME_NAME' mounted at '$MOUNT_PATH'."
-
 # Get datafile from COS
 # echo "Get datafile from COS bucket"
 # ibmcloud cos object-get --bucket ${BUCKET} --key DATAFILE  --region us-east /go/DATAFILE
