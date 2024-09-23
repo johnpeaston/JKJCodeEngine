bf1c9b76e8259ad67137e976e13fb1e615bae40a
John Easton <JKJ@uk.ibm.com>
2024-09-23 11:27:00 +0100

    Cleanup of old files

--- s2i-dockerfile/app.sh
+++ s2i-dockerfile/app.sh
@@ -5,30 +5,26 @@
 
 # env
 #JOB_INDEX=5
-echo "JOB_INDEX= "$JOB_INDEX >>/mnt/OUT
+# echo "JOB_INDEX= "$JOB_INDEX >>/mnt/OUT
 OUTPUTDATAFILE=/mnt/xxx.$JOB_INDEX
 #OUTPUTDATAFILE=./xxx.$JOB_INDEX
 INPUTDATAFILE=/mnt/DATAFILE
 #INPUTDATAFILE=./DATAFILE
-echo "Input will be in: "$INPUTDATAFILE >>/mnt/OUT
-echo "Output will be in: "$OUTPUTDATAFILE >>/mnt/OUT
+# echo "Input will be in: "$INPUTDATAFILE >>/mnt/OUT
+# echo "Output will be in: "$OUTPUTDATAFILE >>/mnt/OUT
 
 # Cleanup
 echo "Cleanup..."
 # rm -f /go/DATAFILE
 rm -f $OUTPUTDATAFILE
 
-# Get datafile from COS
-# echo "Get datafile from COS bucket"
-# ibmcloud cos object-get --bucket ${BUCKET} --key DATAFILE  --region us-east /go/DATAFILE
-
 # check datafile is OK
-echo "cat datafile..." >>/mnt/OUT
-cat $INPUTDATAFILE >>/mnt/OUT
+# echo "cat datafile..." >>/mnt/OUT
+# cat $INPUTDATAFILE >>/mnt/OUT
 
 # Process datafile
-echo "outputfile is " >>/mnt/OUT
-echo $OUTPUTDATAFILE >>/mnt/OUT
+# echo "outputfile is " >>/mnt/OUT
+# echo $OUTPUTDATAFILE >>/mnt/OUT
 
 # Processing datafile...
 while IFS=  read -r line
@@ -43,11 +39,7 @@ do
 done < $INPUTDATAFILE
 
 # Show output file
-cat $OUTPUTDATAFILE  >>/mnt/OUT
-
-# Put output datafile to COS
-# echo "Put output datafile to COS..."
-# ibmcloud cos object-put --bucket ${BUCKET} --key Output.$JOB_INDEX --body ${OUTPUTDATAFILE}  --region us-east
+# cat $OUTPUTDATAFILE  >>/mnt/OUT
 
 exit 0
 
